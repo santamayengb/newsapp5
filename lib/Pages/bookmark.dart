993934f536b57b15993f6a2be1ce20fa4128b362
santamayengb <19mca8232@cuchd.in>
2021-11-29 08:24:34 +0530

    29

--- lib/Pages/bookmark.dart
+++ lib/Pages/bookmark.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class BookmarkPage extends StatefulWidget {
   const BookmarkPage({Key? key}) : super(key: key);
@@ -14,6 +15,21 @@ class _BookmarkPageState extends State<BookmarkPage> {
       appBar: AppBar(
         title: const Text("Bookmark"),
       ),
+      body: FutureBuilder(
+          future: Hive.openBox('NewsBookmarkBox'),
+          builder: (context, snapshot) {
+            final data = snapshot.data;
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                return Text(data.toString());
+              } else {
+                return const Text("No Data");
+              }
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
     );
   }
 }
