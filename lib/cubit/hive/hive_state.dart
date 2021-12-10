c321aed40156488db12db90f2baa8f0707ecc507
santamayengb <19mca8232@cuchd.in>
2021-12-10 08:05:37 +0530

    okay bool not okay

--- lib/cubit/hive/hive_state.dart
+++ lib/cubit/hive/hive_state.dart
@@ -3,7 +3,7 @@ part of 'hive_cubit.dart';
 class HiveState extends Equatable {
   const HiveState({
     required this.dataModel,
-    this.isLiked,
+    this.isLiked = true,
   });
 
   final List<DataModel> dataModel;
